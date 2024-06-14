aacafe40f6f4235f0a7f5ed931e96a32e14cb9b8
Emily Morgan <emorgan921@gmail.com>
2024-06-14 11:49:04 -0400

    Add project files.

---
+++ ClassesExample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassesExample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TQL tql = new TQL();
+        tql.SecretNumber = 1;
+        tql.DisplayClassname();
+        var newSecretNumber = tql.RandomizeSecretNumber(321);
+        Console.WriteLine($"The new secret number is {newSecretNumber}.");
+        tql.DisplayClassname();
+      
+
+    }
+}
+
+internal class TQL {
+    public int SecretNumber {get;set;}
+    public void DisplayClassname() {
+        Console.WriteLine($"Classname is TQL; Secret Number is {SecretNumber}.");
+        }
+
+    public int RandomizeSecretNumber(int RandomNumber) {
+        SecretNumber *= RandomNumber;
+        return SecretNumber;
+    }
+}
